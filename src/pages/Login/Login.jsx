c3887815eadc1054fb411cd89b7b98b054f5a372
dianna1234 <01234dianna@gmail.com>
2023-04-14 22:55:37 +0400

    login user

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,9 +8,37 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  async function handleSubmitLogin(e){
+    e.preventDefault();
+      try {
+        const response = await fetch("http://localhost:3001/login", {
+          method: "POST",
+          body: JSON.stringify({
+            email,
+            password
+          }),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        });
+        const data = await response.json();
+        console.log(data, "data");
+      } catch (err) {
+        console.log(err);
+      }
+      setEmail('');
+      setPassword('');
+    }
+
   return (
     <Container component="main" maxWidth="xs">
         <CssBaseline />
@@ -28,7 +56,7 @@ function Login() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
+          <Box component="form" onSubmit={handleSubmitLogin} noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               required
@@ -38,6 +66,8 @@ function Login() {
               name="email"
               autoComplete="email"
               autoFocus
+              value={email}
+              onChange={(e)=> setEmail(e.target.value)}
             />
             <TextField
               margin="normal"
@@ -48,6 +78,8 @@ function Login() {
               type="password"
               id="password"
               autoComplete="current-password"
+              value={password}
+              onChange={(e)=> setPassword(e.target.value)}
             />
             <Button
               type="submit"
@@ -64,7 +96,7 @@ function Login() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="#" variant="body2" onClick={()=> navigate('/register')}>
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
