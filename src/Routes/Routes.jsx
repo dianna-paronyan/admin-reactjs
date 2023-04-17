70c46a5fa7768d85907a7045aeba6233993743e7
dianna1234 <01234dianna@gmail.com>
2023-04-17 10:56:59 +0400

    add routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,18 +1,34 @@
-import { Route, Routes } from "react-router-dom"
-import Login from "../pages/Login/Login"
-import Register from "../pages/Register/Register"
-import Layout from "../Layouts/Layout"
-
+import { Route, Routes } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Layout from "../Layouts/Layout";
+import UserProvider, { useUserContext } from "../Providers/UserProvider";
+import Home from "../pages/Home";
+import Admin from "../pages/adminPage/Admin";
+import Products from "../components/Products";
+import Users from "../components/Users";
+import CreateProduct from "../components/CreateProduct";
+import EditProduct from "../components/EditProduct";
 
 function AppRoutes() {
+  // const value = useUserContext();
+  // console.log(value);
   return (
-    <Routes>
+    <UserProvider>
+      <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         <Route element={<Layout />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="/admin" element={<Admin/>} />
+          <Route path="/users" element={<Users/>}/>
+          <Route path="/products" element={<Products/>}/>
+          <Route path="/createProduct" element={<CreateProduct/>}/>
+          <Route path="/updateProduct/:id" element={<EditProduct />}/>
         </Route>
-    </Routes>
-  )
+      </Routes>
+    </UserProvider>
+  );
 }
 
-export default AppRoutes
+export default AppRoutes;
