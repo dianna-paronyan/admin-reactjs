491cb0e545e3b5c93c605a82450dbc2292609eba
dianna1234 <01234dianna@gmail.com>
2023-04-19 20:25:59 +0400

    get data from localstorage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Paper, Typography } from "@mui/material";
-import useLogin from "../hooks/useLogin";
 
 function Home() {
+  const user = JSON.parse(localStorage.getItem('user'))
   return (
     <Paper elevation={3} sx={{width:'50%', height:'80px',display:'flex',justifyContent:'center', alignItems:'center'}}>
     <Typography variant="h3" component="h2" sx={{textAlign:'center'}}>
