6ca3c07bc217cae5e33879ba80910ec62a38c186
dianna1234 <01234dianna@gmail.com>
2023-04-19 20:17:47 +0400

    create protected route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -8,21 +8,28 @@ import Products from "../components/Products";
 import Users from "../components/Users";
 import CreateProduct from "../components/CreateProduct";
 import EditProduct from "../components/EditProduct";
+import Categories from "../components/Categories";
+import CreateCategory from "../components/CreateCategory";
+import ProtectedRoute from "./ProtectedRoute";
 
 function AppRoutes() {
+  const user = JSON.stringify(localStorage.getItem('user'));
+
   return (
-      <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        <Route element={<Layout />}>
-          <Route path="/home" element={<Home/>} />
-          <Route path="/admin" element={<Admin/>} />
-          <Route path="/users" element={<Users/>}/>
-          <Route path="/products" element={<Products/>}/>
-          <Route path="/createProduct" element={<CreateProduct/>}/>
-          <Route path="/updateProduct/:id" element={<EditProduct />}/>
-        </Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={user && user.role === 1 ? <Admin /> : <Login/>} />
+      <Route path="/register" element={<Register />} />
+      <Route element={<Layout />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/admin" element={<ProtectedRoute><Admin /></ProtectedRoute>} />
+        <Route path="/users" element={<ProtectedRoute><Users /></ProtectedRoute>} />
+        <Route path="/createProduct" element={<ProtectedRoute><CreateProduct /></ProtectedRoute>} />
+        <Route path="/updateProduct/:id" element={<ProtectedRoute><EditProduct /></ProtectedRoute>} />
+        <Route path="/categories" element={<ProtectedRoute><Categories /></ProtectedRoute>} />
+        <Route path="/createCategory" element={<ProtectedRoute><CreateCategory /></ProtectedRoute>} />
+      </Route>
+    </Routes>
   );
 }
 
