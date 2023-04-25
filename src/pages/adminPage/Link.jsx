d1505deaa072de3cc19c64950a00a9e4511764a6
dianna1234 <01234dianna@gmail.com>
2023-04-25 23:08:40 +0400

    replace Link

--- src/pages/adminPage/Link.jsx
+++
@@ -1,9 +0,0 @@
-import { Link as RouterLink } from "react-router-dom"
-
-function Link(props) {
-  return (
-    <RouterLink  {...props} style={{textDecoration:'none', color:'#000'}} />
-  )
-}
-
-export default Link
